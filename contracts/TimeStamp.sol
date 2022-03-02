797eb2e12d9c06ee463fc2ca48276d039f828f48
Vinay Singh <vinaysingh8866@gmail.com>
2022-03-02 11:41:48 -0500

    Comments added and datatype of gps changed

--- contracts/TimeStamp.sol
+++ contracts/TimeStamp.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.0;
  */
 
  contract TimeStampHash{
-
+    
     struct Timestamp{
          uint totalStamps;
          mapping(bytes32 => bytes32) stamps;
@@ -15,14 +15,18 @@ pragma solidity ^0.8.0;
 
     struct ImageMeta{
         uint time;
-        uint gps;
+        string gps;
     }
     
     mapping(address => Timestamp) public data;
     mapping(bytes32 => address) public fileOwners;
     mapping(bytes32 => ImageMeta) public imageMetaData;
 
-
+    /**
+    * @param stampHash byte32 hash of the data
+    * @param nameHash byte32 hash of the namee of file 
+    * 
+    */
     function insertTimeStamp(bytes32 stampHash, bytes32 nameHash) public {
         uint256 total = data[msg.sender].totalStamps;
         fileOwners[stampHash] = msg.sender;
@@ -30,10 +34,19 @@ pragma solidity ^0.8.0;
         data[msg.sender].stamps[nameHash] = stampHash;
     }
 
+    /**
+    * @param nameHash byte32 hash of the namee of file 
+    * 
+    */
     function getTimeStamp(bytes32 nameHash) public view returns(bytes32 stampHash){
         return data[msg.sender].stamps[nameHash]; 
     }
 
+    /**
+    * @param stampHash byte32 hash of the data
+    * @param nameHash byte32 hash of the namee of file 
+    * 
+    */
     function insertMultiple(bytes32[] memory stampHash,bytes32[] memory nameHash) public {
         require(stampHash.length == nameHash.length);
         uint256 total = data[msg.sender].totalStamps;
@@ -44,11 +57,18 @@ pragma solidity ^0.8.0;
         }
     }
 
+    /**
+    * @param stampHash byte32 hash of data 
+     */
     function getOwner(bytes32 stampHash) public view returns(address owner){
         return fileOwners[stampHash];
     }
 
-    function insertImageMeta(uint time, uint gps, bytes32 stampHash) public {
+    /**
+    * @param time unix time as uint
+    * @param gps uint value of gps coordinates
+    */    
+    function insertImageMeta(uint time,string memory gps, bytes32 stampHash) public {
         fileOwners[stampHash] = msg.sender;
         imageMetaData[stampHash].time = time;
         imageMetaData[stampHash].gps = gps;
