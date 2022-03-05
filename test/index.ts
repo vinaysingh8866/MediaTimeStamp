f0ecef54c765a653b93855f2a1a167fcc1bd7613
Vinay Singh <vinaysingh8866@gmail.com>
2022-03-04 23:57:04 -0500

    Multiple insert test added

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,16 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { TimeStampHash } from "../typechain";
 
 describe("TimeStamp", function () {
-  it("Check Insertion", async function () {
+  let timestamp: TimeStampHash;
+  beforeEach(async function () {
     const TimeStamp = await ethers.getContractFactory("TimeStampHash");
-    const timestamp = await TimeStamp.deploy();
+    timestamp = await TimeStamp.deploy();
     await timestamp.deployed();
+  });
 
+  it("Check Insertion", async function () {
     const insertTimeStamp = await timestamp.insertTimeStamp(
       "0x46696c6531446174610000000000000000000000000000000000000000000000",
       "0x46696c6531000000000000000000000000000000000000000000000000000000"
@@ -17,6 +21,37 @@ describe("TimeStamp", function () {
     expect(
       await timestamp.getTimeStamp(
         "0x46696c6531000000000000000000000000000000000000000000000000000000"
+      )
+    ).to.equal(
+      "0x46696c6531446174610000000000000000000000000000000000000000000000"
+    );
+  });
+
+  it("Check Multiple Insertion", async function () {
+    const insertTimeStamp = await timestamp.insertMultiple(
+      [
+        "0x46696c6531406174910000000000000000000000000000000000000000000000",
+        "0x46696c6531446174610000000000000000000000000000000000000000000000",
+      ],
+      [
+        "0x46696c6511000000000000000000000000000000000000000000000000000000",
+        "0x46696c6530000000000000000000000000000000000000000000000000000000",
+      ]
+    );
+
+    // wait until the transaction is mined
+    await insertTimeStamp.wait();
+    expect(
+      await timestamp.getTimeStamp(
+        "0x46696c6511000000000000000000000000000000000000000000000000000000"
+      )
+    ).to.equal(
+      "0x46696c6531406174910000000000000000000000000000000000000000000000"
+    );
+
+    expect(
+      await timestamp.getTimeStamp(
+        "0x46696c6530000000000000000000000000000000000000000000000000000000"
       )
     ).to.equal(
       "0x46696c6531446174610000000000000000000000000000000000000000000000"
